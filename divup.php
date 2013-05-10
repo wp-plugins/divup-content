d958a248e06095c988079151a216116d24c75235
bastywebb <bastywebb@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-05-10 12:46:42 +0000

    Version 2.0 - use your own start and end wrappers.
    git-svn-id: https://plugins.svn.wordpress.org/divup-content/trunk@710902 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- divup.php
+++ divup.php
@@ -3,7 +3,7 @@
 Plugin Name: DivUp Content
 Plugin URI: http://www.sebastianwebb.co.uk/wordpress-plugins.html
 Description: Client friendly way to separate your WordPress post or page content into divs with (optional) custom CSS classes and ids. Adding the shortcode [divup] in between some content will split the content into 2 separate divs. You can enter as many [divup] shortcodes to a post or page as you like. Great for creating columns of content for magazine style websites while keeping shortcode markup to an absolute minimum. DivUp Content never uses inline styles, but it does automatically give divs fiendishly clever classes like first, last, div-1, div-2, div-3 and div-odd, div-even, mul-3, mul-4 (multiple of 1,2,3,4 etc). Adding 'multiple of' classes to divs is a unique feature of DivUp Content that makes grid layouts with multiple rows a breeze. For instance, the CSS for a 3 column layout (with 2 or more rows) in a 640px content area could be: .divup-wrap { overflow:hidden; } .divup { float:left;width:200px;margin-right:20px; } .mul-3 { margin-right:0; }. For a 6 Column layout, you would just change the CSS to .divup-wrap { overflow:hidden; } .divup { float:left;width:100px;margin-right:8px; } .mul-6 { margin-right:0; }. DivUp Content even has a CSS class solution to multi-row grid layouts with varying column widths. 
-Version: 1.0
+Version: 2.0
 Author: Sebastian Web Design
 Author URI: http://www.sebastianwebb.co.uk
 
@@ -35,8 +35,23 @@ if (!is_admin()) {
 	if (!class_exists('tvr_divup')) {		
 		// define
 		class tvr_divup {
-			
+
+			// total matches
 			var $matches;
+			var $start_wrap_matches;
+			var $end_wrap_matches;
+			// wrapper count trackers
+			var $total_wrap_count = 1;
+			var $wrap_count=1;
+			// div count trackers
+			var $total_count = 2;
+			var $div_count=2;
+			var $diff_count=1;
+			var $global_div_count=1;
+			// store comma extracted values for post_processing
+			var $first_div_strings = array();
+			// store list of divs that are last in wrapper
+			var $last_in_wrap = array();
 			
 			/**
 			* PHP 4 Compatible Constructor
@@ -47,15 +62,18 @@ if (!is_admin()) {
 			*/       
 			function __construct(){
 				// add content filter and shortcode
-				add_filter( 'the_content', array(&$this, 'check_divup'));
+				add_filter( 'the_content', array(&$this, 'check_divup'), 5);
 				add_shortcode('divup', array(&$this, 'support_divup'));
+				add_shortcode('startwrap', array(&$this, 'support_start_divwrap'));
+				add_shortcode('endwrap', array(&$this, 'support_end_divwrap'));
+				add_filter( 'the_content', array(&$this, 'post_shortcode_processing'), 150);
 			} 
 			
 			// check for divup shortcodes
 			function check_divup($content) {
 				$subject = $content;
 				$pattern = '/\[divup/';
-				// if [divup]s exist, create shortcode function
+				// if [divup]s exist, prepare auto classes for wrapper and first div
 				if (preg_match_all($pattern, $subject, $matches)) {
 					$this->matches = count($matches[0]);
 					global $post; 
@@ -66,7 +84,7 @@ if (!is_admin()) {
 					$out);
 					if (strpos($out[1], ',') !== false) {
 						$first_second_id = explode(',', $out[1]);
-						$first_id_str = 'id="'.trim($first_second_id[0]).'" '; 
+						$first_id_str = 'id="'.esc_attr(trim($first_second_id[0])).'" ';
 					}
 					else {
 						$first_id_str = '';
@@ -78,69 +96,174 @@ if (!is_admin()) {
 					if (strpos($out[1], ',') !== false) {
 						$first_second_class = explode(',', $out[1]);
 						$first_class = ' ' . trim($first_second_class[0]); 
-						// check if div_count should be diffped
-						if (strpos($first_class, 'diff') !== false) { 
-							$first_class_str = ' class="divup diff-1 diff-odd first '.esc_attr($first_class).'"';
+						// check if div_count should be diffed
+						if (strpos($first_class, 'diff') !== false) {
+							$d_class = 'diff';
 						}
 						else {
-							$first_class_str = ' class="divup div-1 div-odd first '.esc_attr($first_class).'"';
+							$d_class = 'div';
 						}
+						$first_class_str = ' class="divup '.$d_class.'-1 '.$d_class.'-odd first total-div-1 '.esc_attr
+						($first_class).'"';
 					}
 					else {
-						$first_class_str = ' class="divup div-1 div-odd first"';
+						$first_class_str = ' class="divup div-1 div-odd first total-div-1 "';
 					}
-					
-					// return the formatted content
-					return '<!-- begin divups -->
-					<div id="divup-post-'.$post->ID.'" class="divup-wrap">
-					<div '.$first_id_str.''.$first_class_str.'>
-					' . $content . '
-					</div>
-					</div>
-					<!-- end divup-wrap -->';
+
+					// check for custom opening [divwrap] shortcodes
+					if (preg_match_all('/\[startwrap/', $subject, $matches)) {
+						$this->start_wrap_matches = count($matches[0]);
+						// just return first div markup and content - user has manually added wrapper divs
+						return $content;
+					}
+					// return the formatted content with auto wrapper div
+					else {
+						++$this->global_div_count;
+						// return $content;
+						return '<div id="divup-post-'.$post->ID.'" class="divup-wrap"><div '.$first_id_str.''.$first_class_str.'>' . $content . '</div></div>';
+					}
 				} 
 				return $content;
 			}
-			
-			// replace [divup] shortcodes
-			function support_divup($atts) { 
+
+			// insert last and comma enabled classes
+			function post_shortcode_processing($content) {
+				if (!$this->start_wrap_matches) {
+					return $content;
+				}
+				// add comma enabled ids/classes to first div in wrapper
+				foreach($this->first_div_strings as $div_num => $array) {
+					if ($array['id'] != '') {
+						$content = str_replace('divup-id-'.$div_num.' ', esc_attr($array['id']), $content);
+					}
+					if ($array['class'] != '') {
+						$content = str_replace('total-div-'.$div_num.' ', 'total-div-'.$div_num.' '.$array['class'], $content);
+					}
+				}
+				// add last class
+				foreach ($this->last_in_wrap as $div_num) {
+					$content = str_replace('total-div-'.$div_num, 'last total-div-'.$div_num, $content);
+				}
+				return $content;
+			}
+
+			// replace [divwrap] shortcodes
+			function support_end_divwrap() {
+				// reset div counts
+				$this->total_count = 2;
+				$this->div_count=2;
+				$this->diff_count=1;
+				// store div number in global array
+				$this->last_in_wrap[] = $this->global_div_count-1; // it gets incremented at end of function
+				return '</div>
+				</div>';
+			}
+
+			// replace [startwrap] shortcodes
+			function support_start_divwrap($atts) {
 				// possible attributes
+				$id = '';
+				$class = '';
 				extract( shortcode_atts( array(
 					'id' => '',
 					'class' => '',
 				), $atts ) );
-				
-				static $total_count = 2;
-				static $div_count=2;
-				static $diff_count=1;
-				
-				
+
+				global $post;
+
+
+				/*** resolve final CSS id and class strings ***/
+
+				// check odd/even
+				if ($this->is_odd($this->wrap_count)) {
+					$odd_even = ' divup-wrap-odd';
+				}
+				else {
+					$odd_even = ' divup-wrap-even';
+				}
+
+				// check multiple of
+				$multiples = $this->get_multiples($this->wrap_count);
+
+				// check if it's the last
+				if ($this->total_wrap_count == ($this->start_wrap_matches)) {
+					$last = ' divup-wrap-last';
+				}
+				else {
+					$last = '';
+				}
+
+				// add user defined id if set
+				if ($id != '') {
+					$id_str = ' id="'.esc_attr($id).'"';
+				} else {
+					if ($this->wrap_count == 1) {
+						$id_str = ' id="divup-post-'.$post->ID.'"';
+					} else {
+						$id_str = ' id="divup-post-'.$post->ID.'-'.$this->wrap_count.'"';
+					}
+				}
+
+				$class_str = ' class="divup-wrap divup-wrap-'.$this->wrap_count.' '  . $odd_even . $multiples . $last;
+				// add user defined class(s)
+				if ($class != '') {
+					$class_str.= ' '.esc_attr($class).'"'; // add user class
+				}
+				else {
+					$class_str.= '"';
+				}
+
+				++$this->wrap_count;
+
+				// return the div markup
+				$wrap = "<div{$id_str}{$class_str}>
+							<div id='divup-id-".$this->global_div_count." '
+							class='divup div-1 div-odd first total-div-".$this->global_div_count." '>";
+				++$this->total_wrap_count;
+				++$this->global_div_count;
+				return $wrap;
+			}
+
+			// replace [divup] shortcodes
+			function support_divup($atts) { 
+				// possible attributes
+				$id = '';
+				$class = '';
+				extract( shortcode_atts( array(
+					'id' => '',
+					'class' => '',
+				), $atts ) );
+
 				/*** resolve final CSS id and class strings ***/
 				
-				// check if first div , functionality has been utilised
+				// check if first div , (comma) functionality has been utilised
 				if (strpos($id, ',') !== false) {
 					$first_second_id = explode(',', $id);
 					$id = trim($first_second_id[1]);
+					// save first div in wrapper id for post-processing
+					$this->first_div_strings[$this->global_div_count-1]['id'] = trim($first_second_id[0]);
 				}
 				if (strpos($class, ',') !== false) {
 					$first_second_class = explode(',', $class);
 					$class = trim($first_second_class[1]);
+					// save first div in wrapper class for post-processing
+					$this->first_div_strings[$this->global_div_count-1]['class'] = trim($first_second_class[0]);
 					// if the diff class has been added to the first div reset count to 1, and increment $diff_count
 					if (strpos($first_second_class[0], 'diff') !== false) {
-						--$div_count;
-						++$diff_count;
+						--$this->div_count;
+						++$this->diff_count;
 					}
 				}
-				
+
 				// check odd/even
-				if ($this->is_odd($div_count)) {
+				if ($this->is_odd($this->div_count)) {
 					$odd_even = ' div-odd';
 				}
 				else {
 					$odd_even = ' div-even';
 				}
 				// diff odd/even
-				if ($this->is_odd($diff_count)) {
+				if ($this->is_odd($this->diff_count)) {
 					$diff_odd_even = ' diff-odd';
 				}
 				else {
@@ -148,31 +271,37 @@ if (!is_admin()) {
 				}
 				
 				// check what div is multiple of
-				$multiples = $this->get_multiples($div_count);
-				// diff mulitple of
-				$diff_multiples = $this->get_multiples($diff_count, 'diff');
+				$multiples = $this->get_multiples($this->div_count);
+				// diff multiple of
+				$diff_multiples = $this->get_multiples($this->diff_count, 'diff');
 				
 				// check if it's the last div
-				if ($total_count == ($this->matches+1)) {
+				if ($this->total_count == ($this->matches+1)) {
 					$last = ' last';
 				}
 				else {
 					$last = '';
 				}
-				// diff last - better if total div count on diff div too
+
+				// add global div count
+				$global_count = ' total-div-'.$this->global_div_count.' '; // space is need for post str_replace
 				
 				// add user defined id if set
 				if ($id != '') {
 					$id_str = ' id="'.esc_attr($id).'"';  // add user id
+				} else {
+					$id_str = ' id="divup-id-'.$this->global_div_count.' "';
 				}
 				// form the class attribute
 				if (strpos($class, 'diff') !== false) { // if div_count should be diffped
-					$class_str = ' class="divup diff-'.$diff_count.' ' . $diff_odd_even . $diff_multiples . $last;
-					$diff_count++;
+					$class_str = ' class="divup diff-'.$this->diff_count.' ' . $diff_odd_even . $diff_multiples .
+					$last . $global_count;
+					++$this->diff_count;
 				}
 				else { // normal div
-					$class_str = ' class="divup div-'.$div_count.' '  . $odd_even . $multiples . $last;
-					$div_count++;
+					$class_str = ' class="divup div-'.$this->div_count.' '  . $odd_even . $multiples .
+					$last . $global_count;
+					++$this->div_count;
 				}
 				// add user defined class(s)
 				if ($class != '') {
@@ -187,7 +316,8 @@ if (!is_admin()) {
 				</div>
 				<div{$id_str}{$class_str}>
 				";
-				++$total_count;
+				++$this->total_count;
+				++$this->global_div_count;
 				return $divup;
 			}
 			
@@ -197,8 +327,8 @@ if (!is_admin()) {
 			}
 			
 			// check multiple
-			function get_multiples($div_count, $type='div') {
-				$number = 3;
+			function get_multiples($div_count, $type = 'div') {
+				$number = 3; // odd and even replace mul-1, mul-2
 				$multiple = '';
 				if ($type == 'div') {
 					$mul = 'mul';
@@ -207,7 +337,6 @@ if (!is_admin()) {
 					$mul = 'diff-mul';
 				}
 				while ($number <= $div_count) {
-					
 					if ($div_count % $number == 0) {
 						$multiple.= ' '.$mul.'-'.$number;
 					} 
--- readme.txt
+++ readme.txt
@@ -4,8 +4,8 @@ Contributors: bastywebb
 Donate link: http://www.sebastianwebb.co.uk/wordpress-plugins.html
 Tags: content, columns, column, columnise, column layout, multiple columns, grid, layout, magazine, page, posts, magazine columns, magazine style, magazine layout, float, div, separate, separation, div up, split
 Requires at least: 2.8
-Tested up to: 3.4.1
-Stable tag: 1.0
+Tested up to: 3.5.1
+Stable tag: 2.0
 
 Flexible & elegant way to split post/page content into divs. Divs are given ordinal classes (div-1, div-2 etc). Minimal shortcode. Client Friendly
 
@@ -13,9 +13,11 @@ Flexible & elegant way to split post/page content into divs. Divs are given ordi
 
 Client friendly way to separate your WordPress post or page content into divs with (optional) **custom CSS classes and ids**. Adding the shortcode **[divup]** in between some content will split the content into 2 **separate divs**. 
 
-You can enter as many [divup] shortcodes to a post or page as you like. Great for creating **columns** of content for magazine style websites while keeping shortcode markup to an absolute minimum. DivUp Content never uses inline styles, but it does automatically give divs fiendishly clever classes like first, last, div-1, div-2, div-3, and div-odd, div-even, mul-3, mul-4 (multiple of 1,2,3,4 etc). You can then create your own style rules for the div in style.css  
+You can enter as many [divup] shortcodes to a post or page as you like. Great for creating **columns** of content for magazine style websites while keeping shortcode markup to an absolute minimum. DivUp Content never uses inline styles, but it does automatically give divs fiendishly clever classes like first, last, div-1, div-2, div-3, and div-odd, div-even, mul-3, mul-4 (multiple of 1,2,3,4 etc). You can then create your own style rules for the divs in style.css 
 
-Adding 'multiple of' classes to divs is a unique feature of DivUp Content that makes **grid layouts with multiple rows** a breeze. 
+Adding 'multiple of' classes to divs is a unique feature of DivUp Content that makes **grid layouts with multiple rows** a breeze.
+
+**NEW: You can also add multiple [startwrap] and [endwrap] shortcodes to control how the divs are wrapped in a wrapper div. This overrides the original auto-wrapper functionality if you choose to use it.**
 
 = 3 Column Example CSS =
 
@@ -49,7 +51,7 @@ For a **6 column layout**, you would just change the CSS to:
 }. 
 
 = Advanced Layouts =
-DivUp Content even has a CSS class solution to **multi-row grid layouts with varying column widths**. There is no type of layout for which it would be unsuitable. But a knowledge of CSS is required.
+DivUp Content even has a CSS class solution to **multi-row grid layouts with varying column widths**. There is no type of layout for which it would be unsuitable. **But a knowledge of CSS is required**.
 
 For usage and more info, visit <a title="DivUp Content Docs"  href="http://www.sebastianwebb.co.uk/wordpress-plugins.html" target="_blank">sebastianwebb.co.uk/wordpress-plugins.html</a>.
 
@@ -59,9 +61,78 @@ For usage and more info, visit <a title="DivUp Content Docs"  href="http://www.s
 2. Activate the plugin through the 'Plugins' menu in WordPress
 3. Use the [divup] shortcode in your posts or pages:
    
-The best way to understand how DivUp Content works (including the advanced aspects) is to **copy and paste the following dummy content into a post or page and then inspect the html with firebug** - paying attention to the CSS classes it automatically applies to the divs.
+The best way to understand how DivUp Content works (including the advanced aspects) is to **copy and paste the ONE of the following dummy content examples below into a post or page and then inspect the html with firebug** - paying attention to the CSS classes it automatically applies to the divs.
 
-= Dummy Content: =
+= Dummy Content 1 - The New Way (Add as many [startwrap] and [endwrap] shortcodes as you like) =
+**With verison 2.0, you can now control when the wrapper div that wraps all the divup div starts and ends. You can also separate the divs into multiple wrapper divs. The automatic ordinal classes will start from 1 again for each wrapper (although there is also a gloabl count of all the divs on the page). Use the new [startwrap] shortcode to begin your wrapper and the new [endwrap] shortcode to end your wrapper. Remember that every [startwrap] shortcode requires an accompanying [endwrap] else you are likely to break the layout of your page. The startwrap and endwrap shortcodes are optional, if you don't use them DivUp Content will still function as it always has.**
+
+Some content on this page. Some content on this page. Some content on this page. Some content on this page. Some content on this page. Some content on this page. Some content on this page.
+
+Some content on this page. Some content on this page. Some content on this page. Some content on this page. Some content on this page. Some content on this page. Some content on this page.
+
+[startwrap class="my-wrap-class another-wrap-class"]
+
+Some content on this page. Some content on this page. Some content on this page. Some content on this page. Some content on this page. Some content on this page. Some content on this page.
+
+[divup id='id-a, id-b' class='class-a, class-b class-b2']
+
+Some content on this page. Some content on this page. Some content on this page. Some content on this page. Some content on this page. Some content on this page. Some content on this page.
+
+[divup]
+
+Some content on this page. Some content on this page. Some content on this page. Some content on this page. Some content on this page. Some content on this page. Some content on this page.
+
+[divup]
+
+Some content on this page. Some content on this page. Some content on this page. Some content on this page. Some content on this page. Some content on this page. Some content on this page.
+
+[endwrap]
+
+Some content on this page. Some content on this page. Some content on this page. Some content on this page. Some content on this page. Some content on this page. Some content on this page.
+
+[startwrap id="my-wrap-id" class="hello-wrap"]
+
+Some content on this page. Some content on this page. Some content on this page. Some content on this page. Some content on this page. Some content on this page. Some content on this page.
+
+[divup class='my-class diff ']
+
+Some content on this page. Some content on this page. Some content on this page. Some content on this page. Some content on this page. Some content on this page. Some content on this page.
+
+[divup id='my-id' class='diff my-class2']
+
+Some content on this page. Some content on this page. Some content on this page. Some content on this page. Some content on this page. Some content on this page. Some content on this page.
+
+[divup]
+
+Some content on this page. Some content on this page. Some content on this page. Some content on this page. Some content on this page. Some content on this page. Some content on this page.
+
+[divup]
+
+Some content on this page. Some content on this page. Some content on this page. Some content on this page. Some content on this page. Some content on this page. Some content on this page.
+
+[divup]
+
+Some content on this page. Some content on this page. Some content on this page. Some content on this page. Some content on this page. Some content on this page. Some content on this page.
+
+[divup class='diff']
+
+Some content on this page. Some content on this page. Some content on this page. Some content on this page. Some content on this page. Some content on this page. Some content on this page.
+
+Some content on this page. Some content on this page. Some content on this page. Some content on this page. Some content on this page. Some content on this page. Some content on this page.
+
+[divup]
+
+Some content on this page. Some content on this page. Some content on this page. Some content on this page. Some content on this page. Some content on this page. Some content on this page.
+
+[divup]
+
+Some content on this page. Some content on this page. Some content on this page. Some content on this page. Some content on this page. Some content on this page. Some content on this page.
+
+[endwrap]
+
+
+
+= Dummy Content 2 - The Original Way (let DivUp Content add one wrapper div) =
 Some content on this page. Some content on this page. Some content on this page. Some content on this page. Some content on this page. Some content on this page. Some content on this page.
 
 [divup id='id-a, id-b' class='class-a, class-b class-b2']
@@ -127,3 +198,7 @@ Yes. Just send an email, and I'd be happy to help. Or, if you think the answer t
 4. 2x2 grid (assuming a 640px content area): .divup-wrap { overflow:hidden; } .divup { float:left;width:310px;margin-right:20px;margin-bottom:20px; } 
 .div-even { margin-right:0; }
 5. 3x2 grid (assuming a 640px content area):.divup-wrap { overflow:hidden; } .divup { float:left;width:200px;margin-right:20px;margin-bottom:20px; } .div-1{ width:640px; }.div-1, div-4 { margin-right:0; }. For more advanced layouts, the special 'diff' CSS class has you covered. View the <a title="DivUp Content Docs" href="http://www.sebastianwebb.co.uk/wordpress-plugins.html" target="_blank">DivUp Content docs</a> for more info.
+
+== Changelog == 
+
+With verison 2.0, you can now control when the wrapper div that wraps all the divup div starts and ends. You can also separate the divs into multiple wrapper divs. The automatic ordinal classes will start from 1 again for each wrapper (although there is also a gloabl count of all the divs on the page). Use the new [startwrap] shortcode to begin your wrapper and the new [endwrap] shortcode to end your wrapper. Remember that every [startwrap] shortcode requires an accompanying [endwrap] else you are likely to break the layout of your page. The new [startwrap] and [endwrap] shortcodes are optional, if you don't use them DivUp Content will still function as it always has.
